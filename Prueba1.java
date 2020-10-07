1a9aaa5378e57226f9498736067a67aeb761fca4
Joel-Camacho <71827345+Joel-Camacho@users.noreply.github.com>
2020-10-07 07:27:52 -0500

    Add files via upload

---
+++ Prueba1.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package basesingeniria;
+
+/**
+ *
+ * @author futik
+ */
+public class Prueba1 {
+    public static void main(String[] args) {
+        System.out.println("Buenos días.");
+    }
+}
